2165b4a971b14b8388a71dd59e1c6f28fdfe2d37
mohsenzareii <mohsenzarei291@gmail.com>
2020-06-18 11:18:26 +0430

    add initialization for create admin

---
+++ tools/initialization.js
@@ -0,0 +1,31 @@
+const User = require ('../models/user');
+
+//check existing admin and create admin account
+const initialization = () => {
+   
+    try {
+        const Exist_Admin = await User.findOne ({role : 'admin'});
+        if (Exist_Admin){
+            return console.log('Admin already exists !');
+        }
+    
+        const ADMIN = new User ({
+            firstName : "mohsen" ,
+            lastName : "zarei" ,
+            userName : "mohsen" ,
+            mobile : "09196381290" ,
+            sex : "male" ,
+            role : "admin" ,
+            password : "12345678"
+        });
+     
+        await ADMIN.save();
+        console.log("Admin created");
+    
+    } catch (error) {
+        console.log('Error in intialization function', error);
+    }
+
+}
+
+module.exports = initialization;
