eff0c2620e0fd81450314a2d551d1ad09ae1572f
mohsenzareii <mohsenzarei291@gmail.com>
2020-06-18 10:55:52 +0430

    config express skeleton and add user schema

---
+++ models/user.js
@@ -0,0 +1,58 @@
+const mongoose = require ('mongoose');
+const Schema = mongoose.Schema;
+
+
+//user schema 
+const UserSchema = new Schema ({
+    firstName : {
+        type : String ,
+        required : true ,
+        trim : true ,
+        maxlength : 30 ,
+        minlength : 3
+    },
+    lastName : {
+        type : String ,
+        required : true ,
+        trim : true ,
+        maxlength : 30 ,
+        minlength : 3
+    },
+    userName : {
+        type : String ,
+        required : true ,
+        unique : true ,
+        trim : true ,
+        maxlength : 30 ,
+        minlength : 3 ,
+        lowercase : true
+    },
+    mobile : {
+        type : String ,
+        required : true ,
+        trim : true ,
+        unique : true
+    },
+    password : {
+        type : String ,
+        required : true ,
+        maxlength : 30 ,
+        minlength : 8
+    },
+    sex : {
+        type : String ,
+        required : true ,
+        enum :['male' ,  'female']
+    },
+    role : {
+        type : String ,
+        enum : ['admin' , 'blogger']
+    },
+    createdAt : {
+        type : Date ,
+        required : true ,
+        default : Date.now
+    }
+});
+
+module.exports = mongoose.model ('User' , UserSchema);
